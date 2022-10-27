90dea0a2624952c59051b32364dda3a81d8a7829
caiovitor55 <vcaio6164@gmail.com>
2022-10-27 20:56:44 -0300

    mudando o código (while para for) para futuros instrumentos

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
 
+/*
 while ( contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador]
@@ -17,5 +18,16 @@ while ( contador < listaDeTeclas.length) {
         tocaSom(idAudio)
     }
     contador = contador + 1;
+}
+*/
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
 
 }
